100d1fe8757c7b39f2b7ac0158e0c8d066968a05
Navin Rajkotia <h.navin.rajkotia@accenture.com>
2020-09-30 00:13:33 +0530

    Consuming the Factory class to generate the Products at Runtime

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
+using CodingTest.Purchases;
 namespace CodingTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to online purchase system");
+            
+            ConcretePurchaseFactory cFactory = new ConcretePurchaseFactory();
+
+            do
+            {
+                Console.WriteLine("Enter the Purchase Type");
+
+                string type = Console.ReadLine();
+
+                IPurchase Purchase = cFactory.GetPurchaseType(type);
+
+                var actions = Purchase.Actions();
+
+                actions.ForEach(a=> {Console.WriteLine(a);}); 
+
+                Console.WriteLine("Type Exit to complete the purchase or press Enter to continue");
+            }while(Console.ReadLine().ToLower() != "exit" );
         }
     }
 }
